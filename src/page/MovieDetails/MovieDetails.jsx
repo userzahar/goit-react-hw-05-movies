49cc293dbcbd1f45cb0d9bc14a11397f8ec668ab
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-28 19:34:09 +0300

    Add MovieDetail page

--- src/page/MovieDetails/MovieDetails.jsx
+++ src/page/MovieDetails/MovieDetails.jsx
@@ -11,29 +11,42 @@ export const MovieDetails = () => {
         const product = getProductById(id)
             .then(refs => {
                 console.log(refs.data)
+                console.log("🚀 ~ product:", refs.data)
                 setMovie(refs.data)
+                // movie.genres.map(gen => {
+                 
+            //   })
             })
         .catch(er => console.log(er));
-        console.log("🚀 ~ product:", product)
     }, [])
-    
+    console.log("💦", movie?.genres?.map(gen => {
+        return gen.name;
+    }).join());
   return (
       <main>
           <NavLink to='/'>{`<-`}go Back</NavLink>
       <img src="https://via.placeholder.com/960x240" alt="" />
-      <div>
-        <h2>
-          {/* Product - {product.name} - {id} */}
-        </h2>
+      <section>
+        <h1>
+            {movie.title}
+        </h1>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
-          sunt excepturi nesciunt iusto dignissimos assumenda ab quae cupiditate
-          a, sed reprehenderit? Deleniti optio quasi, amet natus reiciendis
-          atque fuga dolore? Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Impedit suscipit quisquam incidunt commodi fugiat aliquam
-          praesentium ipsum quos unde voluptatum?
-        </p>
-      </div>
+          User score: {movie.vote_average}
+              </p>
+              <h2>Overview</h2>
+              <p>{movie.overview}</p>
+              <h3>Genres</h3>
+              <p>{movie?.genres?.map(gen => {
+        return gen.name;
+    }).join(' ')}</p>
+          </section>
+          <section>
+              <p>Additional information</p>
+              <ul>
+                <li><NavLink>Cast</NavLink></li>
+                <li><NavLink>Reviews</NavLink></li>
+              </ul>
+          </section>
     </main>
   );
 };
